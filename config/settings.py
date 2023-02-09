041ba84d265c9835016d16f7394c307cd3d49d7e
kseniia <kseniia@smartiot.cloud>
2023-02-09 21:35:03 +0200

    cors settings

--- config/settings.py
+++ config/settings.py
@@ -46,9 +46,16 @@ INSTALLED_APPS = [
     "account",
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
+
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
